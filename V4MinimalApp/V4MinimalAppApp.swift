2d71a3b05dba2794bf44d122eae1370a2f15527a
Peter Ham <peterrham@gmail.com>
2024-10-30 09:13:49 -0700

    Initial Commit

---
+++ V4MinimalApp/V4MinimalAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  V4MinimalAppApp.swift
+//  V4MinimalApp
+//
+//  Created by Ham, Peter on 10/30/24.
+//
+
+import SwiftUI
+
+@main
+struct V4MinimalAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
