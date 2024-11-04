5cc43007fcdce337f6557d93427e0740b7a7f188
Peter Ham <peterrham@gmail.com>
2024-11-04 09:27:01 -0800

    This work reasonably well. It handles new lines as the stop word and accumlates text across recognition object "resets". It stores only one line in the database.

--- SpeechRecognition.swift
+++ SpeechRecognition.swift
@@ -12,17 +12,193 @@ class SpeechRecognitionManager: ObservableObject {
     private var recognitionTask: SFSpeechRecognitionTask?
     private let context: NSManagedObjectContext
     
+    // this is the text that we add together until we get a stop word
+    
+    private var  accumulatedText: String = ""
+    
+    // this is the text from the last good recognition result
+    private var  previousText: String = ""
+    
+    
+    
     @Published var incrementalText: String = ""  // Shows incremental speech as it's recognized
     @Published var finalText: String = ""        // Shows finalized speech after "stop" is detected
     
     // Silence detection timer
     private var silenceTimer: Timer?
-    private let silenceThreshold: TimeInterval = 2.0  // Adjust as needed
+    private let silenceThreshold: TimeInterval = 12.0  // Adjust as needed
+    
+    func debugPrint(_ str: String) {
+    }
     
     init(context: NSManagedObjectContext) {
+        
+        print("inside init()")
         self.context = context
         requestMicrophonePermission()
         startListening()
+    }
+    
+    func setupAudioSessionObserver() {
+        NotificationCenter.default.addObserver(self, selector: #selector(handleAudioSessionInterruption(_:)), name: AVAudioSession.interruptionNotification, object: AVAudioSession.sharedInstance())
+    }
+    
+    @objc func handleAudioSessionInterruption(_ notification: Notification) {
+        
+        debugPrint("handleAudioSessionInterruption")
+        guard let userInfo = notification.userInfo,
+              let interruptionTypeValue = userInfo[AVAudioSessionInterruptionTypeKey] as? UInt,
+              let interruptionType = AVAudioSession.InterruptionType(rawValue: interruptionTypeValue) else {
+            return
+        }
+        
+        if interruptionType == .began {
+            debugPrint("Audio session interruption began. Pausing recognition.")
+            recognitionTask?.cancel() // Cancel the task if an interruption begins
+        } else if interruptionType == .ended {
+            debugPrint("Audio session interruption ended.")
+        }
+    }
+    
+    func logWithTimestamp(_ message: String) {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd HH:mm:ss.SSS"
+        let timestamp = dateFormatter.string(from: Date())
+        debugPrint("[\(timestamp)] \(message)")
+    }
+    
+    let index = 0;
+    
+    func printTranscriptionSegments(transcription: SFTranscription) {
+        for (index, segment) in transcription.segments.enumerated() {
+            debugPrint("index: \(index)")
+            debugPrint("Text: \(segment.substring)")
+            debugPrint("Range: \(segment.substringRange)")
+            debugPrint("Timestamp: \(segment.timestamp) seconds")
+            debugPrint("Duration: \(segment.duration) seconds")
+            debugPrint("Confidence: \(segment.confidence)")
+            debugPrint("-----------------------")
+            
+            if (index == 0) {
+                if segment.timestamp == 0 {
+                    debugPrint("ignore this one")
+                } else {
+                    debugPrint("keep this one")
+                }
+            }
+        }
+    }
+    
+    
+    func processRecognitionResult(recognitionResult: SFSpeechRecognitionResult?, error: Error?)
+    {
+        self.logWithTimestamp("got recognition result")
+        
+        
+        debugPrint("recognitionTask: \(self.recognitionTask)")
+        debugPrint("recognitionTask.state: \(self.recognitionTask!.state)")
+        
+        // debugPrint(recognitionTask!)
+        
+        debugPrint("Recognition Task Properties:")
+        debugPrint("----------------------------")
+        debugPrint("State: \(recognitionTask!.state)")
+        debugPrint("Is Finishing: \(recognitionTask!.isFinishing)")
+        debugPrint("Is Cancelled: \(recognitionTask!.isCancelled)")
+        
+        // Print the current state of the task
+        debugPrint("Recognition Task State:")
+        switch recognitionTask!.state {
+        case .starting:
+            debugPrint("The task is starting.")
+        case .running:
+            debugPrint("The task is running.")
+        case .finishing:
+            debugPrint("The task is finishing.")
+        case .canceling:
+            debugPrint("The task is canceling.")
+        case .completed:
+            debugPrint("The task is completed.")
+        @unknown default:
+            debugPrint("Unknown state.")
+        }
+        
+        
+        
+        
+        // debugPrint(result)
+        if let result = recognitionResult {
+            
+            // if this one starts at timestamp 0, then I know the the previous segments have been trimmed
+            // then accumlate the previous one
+            
+            /* This one does not seem to quite work ...
+             if (result.bestTranscription.segments[0].timestamp == 0) {
+             accumulatedText = accumulatedText + " " + previousText
+             print("got a restart")
+             // return
+             }
+             */
+            
+            var isReset:Bool = false
+            
+            if (result.bestTranscription.segments.first?.confidence ?? 0.0) > 0.0 {
+                accumulatedText = accumulatedText + " " + previousText
+                print("got zero confidence")
+                isReset = true
+            }
+            
+            
+            
+            
+            debugPrint("result.isFinal: \(result.isFinal)")
+            
+            
+            /*
+             for segment in result.bestTranscription.segments {
+             // debugPrint("Recognized word: \(segment.substring), Confidence: \(segment.confidence)")
+             debugPrint("Recognized word: \(segment.substring)")
+             }
+             for transcription in result.transcriptions {
+             debugPrint("Alternative transcription: \(transcription.formattedString)")
+             }
+             */
+            
+            // printTranscriptionSegments(transcription: result.bestTranscription)
+            
+            // let stopWord = "go"
+            // let stopWord = "new line"
+            let stopWord = "\n"
+            
+            
+            let recognizedText = result.bestTranscription.formattedString
+            debugPrint(recognizedText)
+            
+            self.incrementalText = recognizedText
+            previousText = recognizedText
+            self.finalText = accumulatedText
+            
+            if recognizedText.contains(stopWord) {
+                
+                if isReset {
+                    let strippedText = recognizedText.replacingOccurrences(of: stopWord, with: "")
+                    self.saveRecognizedText(accumulatedText)  // Save text when final result is received'
+                    accumulatedText = ""
+                    debugPrint(strippedText)
+                    // self?.stopListening()                     // Stop and clear audio processing
+                    // self?.startListening()
+                }                    // Restart listening after detecting "stop"
+            } else if result.isFinal {
+                debugPrint("got isFinal")
+                
+                self.startListening()
+            }
+        } else if let error = error {
+            debugPrint("Recognition error: \(error.localizedDescription)")
+            self.stopListening()
+            self.startListening()
+        }
+        
     }
     
     // Starts listening and sets up the speech recognition task
@@ -30,78 +206,77 @@ class SpeechRecognitionManager: ObservableObject {
         // Ensure permissions are granted
         guard SFSpeechRecognizer.authorizationStatus() == .authorized,
               AVAudioSession.sharedInstance().recordPermission == .granted else {
-            print("Permissions not granted.")
+            debugPrint("Permissions not granted.")
             return
         }
+        
+        
+        
         
         // Configure and activate audio session
         let audioSession = AVAudioSession.sharedInstance()
         do {
-            try audioSession.setCategory(.record, mode: .measurement, options: .duckOthers)
+            try audioSession.setCategory(.playAndRecord, mode: .measurement, options: .duckOthers)
             try audioSession.setActive(true, options: .notifyOthersOnDeactivation)
+            // try audioSession.setPreferredIOBufferDuration(0.01)  // Set a lower buffer duration for real-time processing
+            try audioSession.setPreferredIOBufferDuration(100)
         } catch {
-            print("Audio session setup error: \(error)")
+            debugPrint("Audio session setup error: \(error)")
             return
         }
+        
+        setupAudioSessionObserver()
         
         // Reset and create a new recognition task
         clearRecognitionTask()
         recognitionRequest = SFSpeechAudioBufferRecognitionRequest()
         recognitionRequest?.shouldReportPartialResults = true
         
+        // speechRecognizer?.defaultTaskHint = .dictation
+        
+        
         // Start the recognition task
         recognitionTask = speechRecognizer?.recognitionTask(with: recognitionRequest!) { [weak self] result, error in
-            print("got recognition result")
-            if let result = result {
-                let recognizedText = result.bestTranscription.formattedString.lowercased()
-                self?.incrementalText = recognizedText
-                if recognizedText.contains("stop") {
-                    self?.finalText = recognizedText
-                    self?.saveRecognizedText(recognizedText)  // Save text after detecting "stop"
-                    self?.stopListening()                     // Stop and clear audio processing
-                    // self?.startListening()                    // Restart listening after detecting "stop"
-                } else if result.isFinal {
-                    self?.finalText = recognizedText
-                    self?.saveRecognizedText(recognizedText)  // Save text when final result is received
-                    // self?.startListening()
-                }
-            } else if let error = error {
-                print("Recognition error: \(error.localizedDescription)")
-                self?.stopListening()
-                self?.startListening()
-            }
+            
+            self?.processRecognitionResult(recognitionResult: result!, error: error)
+            
         }
         
         // Attach audio input node
+        
         let inputNode = audioEngine.inputNode
         let recordingFormat = inputNode.outputFormat(forBus: 0)
-        inputNode.installTap(onBus: 0, bufferSize: 1024, format: recordingFormat) { [weak self] buffer, when in
-            // print("got buffer")
+        
+        
+        inputNode.installTap(onBus: 0, bufferSize: 1024 * 1024, format: recordingFormat) { [weak self] buffer, when in
+            // debugPrint("got buffer")
             self?.recognitionRequest?.append(buffer)
         }
+        
         
         // Start audio engine
         audioEngine.prepare()
         do {
             try audioEngine.start()
         } catch {
-            print("Audio engine could not start: \(error)")
+            debugPrint("Audio engine could not start: \(error)")
         }
         
-       // startSilenceTimer()  // Start silence detection timer
+        // startSilenceTimer()  // Start silence detection timer
     }
     
     private func clearRecognitionTask() {
-        recognitionTask?.cancel()
+        // recognitionTask?.cancel()
         recognitionTask = nil
         recognitionRequest = nil
     }
     
     func stopListening() {
+        debugPrint("inside stopListening")
         recognitionRequest?.endAudio()  // Signal end of audio to finalize recognition
         audioEngine.stop()              // Stop the audio engine to free resources
-        audioEngine.inputNode.removeTap(onBus: 0)  // Remove the tap to stop capturing audio
-
+        //audioEngine.inputNode.removeTap(onBus: 0)  // Remove the tap to stop capturing audio
+        
         clearRecognitionTask()
         resetSilenceTimer()
     }
@@ -111,12 +286,12 @@ class SpeechRecognitionManager: ObservableObject {
         let newEntry = RecognizedTextEntity(context: context) // Replace RecognizedTextEntity with your Core Data entity name
         newEntry.content = text
         newEntry.timestamp = Date()  // Add a timestamp if needed
-
+        
         do {
             try context.save()
-            print("Text saved successfully: \(text)")
+            debugPrint("Text saved successfully: \(text)")
         } catch {
-            print("Failed to save text: \(error.localizedDescription)")
+            debugPrint("Failed to save text: \(error.localizedDescription)")
         }
     }
     
@@ -124,6 +299,7 @@ class SpeechRecognitionManager: ObservableObject {
     private func startSilenceTimer() {
         resetSilenceTimer()
         silenceTimer = Timer.scheduledTimer(withTimeInterval: silenceThreshold, repeats: false) { [weak self] _ in
+            self?.debugPrint("timer fired")
             self?.stopListening()  // Stop if no audio is detected within threshold
         }
     }
